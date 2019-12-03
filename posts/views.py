e58893d8e6361eccb37f7e4d6920b920c2ba207a
Vikas Bhandary <vksbhandary@gmail.com>
2019-12-03 12:34:28 +0000

    Updated home template

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    template = 'base.html'
+    template = 'home.html'
     context = {}
     return render(request, template,context)
