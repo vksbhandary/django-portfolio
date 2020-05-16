1c6c63818d51a07409c3f30453a0031e16a922e9
Vikas Bhandary <vksbhandary@gmail.com>
2020-05-16 14:21:35 +0545

    Added option to hide project menu on website

--- usersettings/models.py
+++ usersettings/models.py
@@ -86,6 +86,7 @@ class SiteSetting(IndexedTimeStampedModel):
     defprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE ,related_name ='user_main', verbose_name = "Default Profile")
     maxblog = models.IntegerField(verbose_name = "Max Blog on home")
     blog_featured_active = models.BooleanField(verbose_name = "Activate Blog featured images", default=False)
+    project_menu_active = models.BooleanField(verbose_name = "Activate Projects menu", default=False)
     siteurl = models.URLField(max_length=1024, verbose_name = "Website url",blank=True,default=None, null=True)
     sitetitle = models.CharField(max_length=150,unique=True, verbose_name = "Site title")
     contact_msg = models.CharField(max_length=512, blank=True, default=None, null=True,  verbose_name = "Contact Message")
